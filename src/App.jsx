9ad5c1c75b4c608b4235f862a6a04aa7ddf48e93
unknown <lpreveton@gmail.com>
2024-07-28 20:20:11 -0700

    add react route and impelement

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./pages/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [nav, setNav] = useState("");
   return (
     <div className="app">
-      <Navbar set={setNav} nav={nav} />
-      <Main setNav={setNav} />
+      <BrowserRouter>
+        <Navbar set={setNav} nav={nav} />
+        <Routes>
+          <Route path="*" />
+          <Route path="/" element={<Main setNav={setNav} />} />
+          <Route path="/commands" />
+          <Route path="/team" />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,17 +1,15 @@
 import Hero from "../../components/Hero/Hero";
 import Features from "../../components/Features/Features";
 import ActionSection from "../../components/ActionSection/ActionSection";
-import Footer from "../../components/Footer/Footer";
 
 import "./Main.scss";
 
-export default function Main({setNav}) {
+export default function Main({ setNav }) {
   return (
     <div className="mainPage">
       <Hero setNav={setNav} />
       <Features />
       <ActionSection />
-      <Footer />
     </div>
   );
 }
