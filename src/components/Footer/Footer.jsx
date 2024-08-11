0e7c032fa7ab45bcbfcde72b54082d92adb08161
unknown <lpreveton@gmail.com>
2024-08-10 18:33:52 -0700

    finish v2 footbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,20 +5,25 @@ import "./Footer.scss";
 export default function Footer({ top }) {
   return (
     <footer className="footer">
-      <div className="footer__logo">Able</div>
-      <div className="footer__right">
+      <div className="footer__container">
+        <div className="footer__logo">
+          Ablebot<p>@2024 Ablebot</p>
+        </div>
+        <div className="footer__links">
+          <Link className="footer__link" to="/team">
+            Team
+          </Link>
+          <Link className="footer__link">Help</Link>
+          <Link className="footer__link" to="/commands">
+            Commands
+          </Link>
+        </div>
         <div
           className="footer__goTop"
           onClick={() => top.current.scrollIntoView({ behavior: "smooth" })}
         >
           <img src={arrow} alt="arrow" />
           <span>Go to top</span>
-        </div>
-        <div className="footer__links">
-          <Link to="/team">Team</Link>
-          <Link>Help</Link>
-          <Link to="/commands">Commands List</Link>
-          <Link>FAQ</Link>
         </div>
       </div>
     </footer>
