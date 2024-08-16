b45c856a3e51a234aafaab51e80367d96b630095
unknown <lpreveton@gmail.com>
2024-08-16 10:33:40 -0700

    some final style changes

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,8 +1,8 @@
 import "./Team.scss";
 import Footer from "../../components/Footer/Footer";
-import sample from "../../assets/sample.png";
 import raven from "../../assets/team/raven.jpg";
 import dan from "../../assets/team/dan.png";
+import azka from "../../assets/team/azka.png";
 import github from "../../assets/icons/github.png";
 import linkedin from "../../assets/icons/linkedin.png";
 import email from "../../assets/icons/email.png";
@@ -51,7 +51,8 @@ export default function Team() {
       name: "Dan",
       position: "Organizer, ML Engineer",
       avatar: dan,
-      linkedin: "https://www.linkedin.com/in/ravenlei/",
+      bg: "#83A3E0",
+      linkedin: "https://www.linkedin.com/in/-danhan-/",
       github: "https://github.com/lpandccxr",
       email: "geoman970@gmail.com",
       dec: "Passionate engineer, dedicated to creating the best products with the most impact. I work on the backend and ML.",
@@ -60,6 +61,7 @@ export default function Team() {
       name: "Raven",
       position: "Lead Frontend Developer",
       avatar: raven,
+      bg: "",
       linkedin: "https://www.linkedin.com/in/raven-lei/",
       github: "https://github.com/lpandccxr",
       email: "lpreveton@gmail.com",
@@ -68,11 +70,12 @@ export default function Team() {
     {
       name: "Azka",
       position: "UX/UI Developer",
-      avatar: sample,
-      linkedin: "https://www.linkedin.com/in/ravenlei/",
+      avatar: azka,
+      bg: "#F52578",
+      linkedin: "",
       github: "",
-      email: "lpreveton@gmail.com",
-      dec: "You can see how this is so much nicer to look at. You will get the whitespace included in the string, but since we're just creating HTML markup, it doesn't really matter.",
+      email: "azkaabokhari27@gmail.com",
+      dec: "I designed a cohesive brand identity for Able Bot, collaborated with the team on ideas and variations for an intuitive user experience.",
     },
   ];
   return (
@@ -89,8 +92,10 @@ export default function Team() {
         >
           <div className="team__request">
             <span>
-              Get to Know the Proficient <span>Team</span> Behind
-              <span> Able</span>.
+              Get to Know the{" "}
+              <span style={{ color: "#83A3E0" }}> Proficient </span>
+              <span style={{ color: "#C5F73A" }}>Team</span> Behind
+              <span style={{ color: "#F52578" }}> Able</span>.
             </span>
             <p>
               We&apos; d love to hear what you think about <span>Able</span>
@@ -129,16 +134,30 @@ export default function Team() {
                   damping: 50,
                 }}
               >
-                <img src={member.avatar} alt="avatar" />
+                <img
+                  src={member.avatar}
+                  alt="avatar"
+                  style={
+                    member.bg.length > 0
+                      ? {
+                          backgroundColor: `${member.bg}`,
+                        }
+                      : {}
+                  }
+                />
                 <div className="team__dec">
                   <div className="team__position">
                     <div className="team__social">
-                      <a href={member.github} target="_blank">
-                        <img src={github} alt="github" />
-                      </a>
-                      <a href={member.linkedin} target="_blank">
-                        <img src={linkedin} alt="linkedin" />
-                      </a>
+                      {member.github.length > 0 && (
+                        <a href={member.github} target="_blank">
+                          <img src={github} alt="github" />
+                        </a>
+                      )}
+                      {member.linkedin.length > 0 && (
+                        <a href={member.linkedin} target="_blank">
+                          <img src={linkedin} alt="linkedin" />
+                        </a>
+                      )}
                       <a href={`mailto: ${member.email}`} target="_blank">
                         <img src={email} alt="email" />
                       </a>
